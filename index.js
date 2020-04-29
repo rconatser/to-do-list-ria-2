306b73c116f1596b8ecc323b848f97713da00fe0
Rachel Conatser <10806252@my.uvu.edu>
2020-04-28 20:03:03 -0600

    added CORS package/info

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+import cors from 'cors';
 
 const taskRoutes = require('./routes/task');
 const userRoutes = require('./routes/user');
@@ -12,9 +13,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors());
 app.use('/', taskRoutes);
 app.use('/user', userRoutes);
+
 
 mongoose
     .connect('mongodb+srv://dgm4790:3tTMvKF9MnKMFBd9@cluster0-wtpya.mongodb.net/app?retryWrites=true&w=majority')
